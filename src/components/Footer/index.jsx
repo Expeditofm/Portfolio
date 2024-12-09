ad1e93efdea25544070f8b1b7a010182512e4599
Expeditofm <expeditofm@hotmail.com>
2024-12-08 22:16:48 -0300

    Portifolio

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Criado por Expedito Mendonça 2024
+        </footer>
+    )
+}
+
+export default Footer
